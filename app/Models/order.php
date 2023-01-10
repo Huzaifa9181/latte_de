1126f34f525e7c05dd59d6447059e38b0fbd2533
Huzaifa Ahmed <huzaifaahmed9181@gmail.com>
2023-01-11 04:34:25 +0500

    Orders Data Model

---
+++ app/Models/order.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class order extends Model
+{
+    use HasFactory;
+    function insert($data){
+        $order = new order;
+        $order->order_number = $data['order_number'];
+        $order->user_name = $data['user_name'];
+        $order->user_id = $data['user_id'];
+        $order->products_id = json_encode($data['product_id']);
+        $save = $order->save();
+        if($save){
+            return 1;
+        }
+    }
+}
