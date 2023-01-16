b9134355f9c6a2dd7e8692b0630af5a46a393ed9
Huzaifa Ahmed <huzaifaahmed9181@gmail.com>
2023-01-16 13:38:30 +0500

    Payment Controller (cash on delivery , card payment)

--- app/Http/Controllers/paymentController.php
+++ app/Http/Controllers/paymentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\order;
+use Illuminate\Support\Facades\Mail;
 
 class paymentController extends Controller
 {
@@ -25,15 +26,32 @@ class paymentController extends Controller
             'user_id' => session('id'),
             'product_id'=>$p_id
         ];
-        $order = $this->model->insert($data);
+
+        $total =0;
+        foreach(session("add_to_cart") as $key => $val){
+            $total = $total + intval($val['quantity'])*p_find($val['id'])['price'];
+        }
+
+        $dataview = [
+            'order_number' => $randomNumber,
+            'user_name' => session('name'),
+            'user_id' => session('id'),
+            'product_id'=>$p_id,
+            'total'=>$total
+        ];
+        $order = $this->model->insert($dataview);
         if($order == 1){
             $req->session()->forget('add_to_cart');
-            // $req->session()->flush();
-            return view('payment_details');
+            $user['to'] = session('email');
+            Mail::send('payment_details', $dataview, function ($messages) use ($user) {
+                $messages->to($user['to']);
+                $messages->subject('Your Order Successfully Through By Latte De Admin Wait For Her Reply');
+            });
         }
     }
 
-    function delivery(){
+    // public $total = 0;
+    function delivery(Request $req){
         $randomNumber = random_int(100000, 999999);
         $session = session("add_to_cart");
         $p_id = array_column($session,"id");
@@ -43,11 +61,29 @@ class paymentController extends Controller
             'user_id' => session('id'),
             'product_id'=>$p_id
         ];
-        $order = $this->model->insert($data);
+
+        $total =0;
+        foreach(session("add_to_cart") as $key => $val){
+            $total = $total + intval($val['quantity'])*p_find($val['id'])['price'];
+        }
+
+        $dataview = [
+            'order_number' => $randomNumber,
+            'user_name' => session('name'),
+            'user_id' => session('id'),
+            'product_id'=>$p_id,
+            'total'=>$total
+
+        ];
+        $order = $this->model->insert($dataview);
         if($order == 1){
-            return view('payment_details');
+            $req->session()->forget('add_to_cart');
+            $user['to'] = session('email');
+            Mail::send('payment_details', $dataview, function ($messages) use ($user) {
+                $messages->to($user['to']);
+                $messages->subject('Your Order Successfully Through By Latte De Admin Wait For Her Reply');
+            });
+            return view("add_to_cart");
         }
     }
-
-    
 }
